9cd467dae0c7931a47384902a62284c2427bab4a
Dexy98 <davidecandia@live.it>
2023-07-16 15:48:47 +0200

    da aggiustare la nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <nav className ="navbar mb-4 flex align-center justify-center mb-3">
+    <nav className ="navbar fissa mb-4 flex align-center justify-center mb-3">
         <a href="#section-1" className="navbar-links">Home</a>
         <a href="#section-2" className="navbar-links">Competenze</a>
         <a href="#section-3" className="navbar-links">Progetti</a>
