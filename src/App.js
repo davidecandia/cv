8c5de9b6f03077a2b61f189458b470e0e4dc8e01
Dexy98 <davidecandia@live.it>
2023-07-20 12:15:25 +0200

    navbar aggiustata

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Competenze from './components/Competenze';
 import Progetti from './components/Progetti';
-import { useState } from 'react';
 import data from './data';
 import Contatti from './components/Contatti';
 import Footer from './components/Footer';
 
 function App() {
+  const [progetti, setprogetti] = useState(data);
+  const [showNavbar, setShowNavbar] = useState(false);
 
-  const [progetti, setprogetti] = useState(data)
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const viewportHeight = window.innerHeight;
+
+      // Imposta la visibilità della navbar in base allo scroll
+      setShowNavbar(scrollY >= viewportHeight);
+    };
+
+    // Aggiungi l'event listener allo scroll
+    window.addEventListener('scroll', handleScroll);
+
+    // Rimuovi l'event listener quando il componente viene smontato
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-    <Home />
-    <Nav />
-    <Competenze />
-    <Progetti data={progetti}/>
-    <Contatti />
-    <Footer />
+      <Home />
+      <Nav className={showNavbar ? 'show' : ''} />
+      <Competenze />
+      <Progetti data={progetti} />
+      <Contatti />
+      <Footer />
     </>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const Nav = () => {
+const Nav = ({ className }) => {
   return (
-    <nav className ="navbar fissa mb-4 flex align-center justify-center mb-3">
+    <nav className={`navbar fissa mb-4 flex align-center justify-center mb-3 ${className}`}>
         <a href="#section-1" className="navbar-links">Home</a>
         <a href="#section-2" className="navbar-links">Competenze</a>
         <a href="#section-3" className="navbar-links">Progetti</a>
         <a href="#section-4" className="navbar-links">Contatti</a>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
+
