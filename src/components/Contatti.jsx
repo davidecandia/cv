bda7f6a680e731ad3f3ec1d2855296dabe6e1e77
Dexy98 <davidecandia@live.it>
2023-08-22 14:41:38 +0200

    aggionto emailjs invio form

--- src/components/Contatti.jsx
+++ src/components/Contatti.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contatti = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_sxzsyln', 'template_0rbt4zf', form.current, 'o_j5KqBPPXixnP4zt')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <section class="section-4 flex direction-column align-center justify-center " id="section-4">
         <h1 class="section-titolo titolo-section-4 mb-3">Contatti</h1>
-        <form class="form flex direction-column">
+        {/* <form class="form flex direction-column">
             <input type="text" placeholder="Nome"/>
             <input type="email" placeholder="E-mail"/>
             <textarea placeholder="Testo"></textarea>
             <input type="submit" value="Invia" class="btn cursor-pointer"/>
-        </form>
+        </form> */}
+          <form ref={form} onSubmit={sendEmail} class="form flex direction-column">
+            <label>Nome</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Messaggio</label>
+            <textarea name="message" />
+            <input type="submit" value="INVIA" class="btn cursor-pointer"/>
+          </form>
 
     </section>
   )
