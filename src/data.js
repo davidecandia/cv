94c946f54804678172d416d80613ea704f1c9df6
Dexy98 <davidecandia@live.it>
2023-08-30 15:00:06 +0200

    Aggiunto nuovo progetto movies

--- src/data.js
+++ src/data.js
@@ -2,38 +2,47 @@ import progetto1_nuovo from "./assets/image/progetto1_nuovo.png"
 import progetto2_nuovo from "./assets/image/progetto2_nuovo.jpg"
 import progetto3_nuovo from "./assets/image/progetto3_nuovo.jpg"
 import progetto4_nuovo from "./assets/image/progetto4_nuovo.jpg"
+import progetto5_nuovo from "./assets/image/progetto5_nuovo.png"
 
 
 
 const data = [
-    {
+   {
       id: 1,
+      name: 'Movies',
+      descrizione: 'sviluppato con React e tailwind per il css con diverse chiamate API TMDB ',
+      image: progetto5_nuovo,
+      url: 'https://dexy98.github.io/movies/',
+    },
+    {
+      id: 2,
+      name: 'meteo',
+      descrizione: 'React js con API openweathermap',
+      image: progetto2_nuovo,
+      url: 'https://dexy98.github.io/meteo/',
+    },
+    {
+      id: 3,
       name: 'sinisky',
       descrizione: 'React js',
       image: progetto4_nuovo,
       url: 'https://dexy98.github.io/sinisky/',
     },
     {
-      id: 2,
-      name: 'meteo',
-      descrizione: 'React js',
-      image: progetto2_nuovo,
-      url: 'https://dexy98.github.io/meteo/',
-    },
-    {
-      id: 3,
+      id: 4,
       name: 'Lista',
       descrizione: 'React js',
       image: progetto1_nuovo,
       url: 'https://dexy98.github.io/lista/'
     },
     {
-      id: 4,
+      id: 5,
       name: 'tecnostampa',
       descrizione: 'React js',
       image: progetto3_nuovo,
       url: 'https://dexy98.github.io/tecnostampa/',
     },
+   
     // Aggiungi altri progetti qui...
   ];
 
