9eaa3211a0b3ab7e27755cb7bae919fec4893408
Dexy98 <davidecandia@live.it>
2023-09-20 11:18:35 +0200

    nuovo progetto

--- src/data.js
+++ src/data.js
@@ -5,61 +5,69 @@ import progetto4_nuovo from "./assets/image/progetto4_nuovo.jpg"
 import progetto5_nuovo from "./assets/image/progetto5_nuovo.png"
 import progetto6_nuovo from "./assets/image/progetto6_nuovo.png"
 import progetto7_nuovo from "./assets/image/progetto7_nuovo.jpg"
+import progetto8_nuovo from "./assets/image/progetto8_nuovo.png"
 
 
 
 const data = [
-    {
+  {
+    id: 8,
+    name: 'Cocktail',
+    descrizione: 'Sviluppato con React e Typescript. Tailwind per il css. API gestita con RTK query. ',
+    image: progetto8_nuovo,
+    url: 'https://cocktailcreationshub.netlify.app/',
+  },
+  {
     id: 7,
     name: 'Dog',
     descrizione: 'Sviluppato con React e Typescript. Tailwind per il css. API gestita con RTK query. ',
     image: progetto7_nuovo,
     url: 'https://dexy98.github.io/dog/',
-    },
-   {
-      id: 6,
-      name: 'E-shop',
-      descrizione: 'Sviluppato con React e tailwind per il css. Fake Store API gestita con RTK query. Gestione degli stati con redux toolkit e typescript per migliore gestione del codice ',
-      image: progetto6_nuovo,
-      url: 'https://dexy98.github.io/e-shop/',
-   },
-   {
-      id: 5,
-      name: 'Movies',
-      descrizione: 'sviluppato con React e tailwind per il css con diverse chiamate API TMDB ',
-      image: progetto5_nuovo,
-      url: 'https://dexy98.github.io/movies/',
-    },
-    {
-      id: 4,
-      name: 'meteo',
-      descrizione: 'React js con API openweathermap',
-      image: progetto2_nuovo,
-      url: 'https://dexy98.github.io/meteo/',
-    },
-    {
-      id: 3,
-      name: 'sinisky',
-      descrizione: 'React js',
-      image: progetto4_nuovo,
-      url: 'https://dexy98.github.io/sinisky/',
-    },
-    {
-      id: 2,
-      name: 'Lista',
-      descrizione: 'React js',
-      image: progetto1_nuovo,
-      url: 'https://dexy98.github.io/lista/'
-    },
-    {
-      id: 1,
-      name: 'tecnostampa',
-      descrizione: 'React js',
-      image: progetto3_nuovo,
-      url: 'https://dexy98.github.io/tecnostampa/',
-    },
-   
-    // Aggiungi altri progetti qui...
-  ];
+  },
+  {
+    id: 6,
+    name: 'E-shop',
+    descrizione: 'Sviluppato con React e tailwind per il css. Fake Store API gestita con RTK query. Gestione degli stati con redux toolkit e typescript per migliore gestione del codice ',
+    image: progetto6_nuovo,
+    url: 'https://dexy98.github.io/e-shop/',
+  },
+  {
+    id: 5,
+    name: 'Movies',
+    descrizione: 'sviluppato con React e tailwind per il css con diverse chiamate API TMDB ',
+    image: progetto5_nuovo,
+    url: 'https://dexy98.github.io/movies/',
+  },
+  {
+    id: 4,
+    name: 'meteo',
+    descrizione: 'React js con API openweathermap',
+    image: progetto2_nuovo,
+    url: 'https://dexy98.github.io/meteo/',
+  },
+  {
+    id: 3,
+    name: 'sinisky',
+    descrizione: 'React js',
+    image: progetto4_nuovo,
+    url: 'https://dexy98.github.io/sinisky/',
+  },
+  {
+    id: 2,
+    name: 'Lista',
+    descrizione: 'React js',
+    image: progetto1_nuovo,
+    url: 'https://dexy98.github.io/lista/'
+  },
+  {
+    id: 1,
+    name: 'tecnostampa',
+    descrizione: 'React js',
+    image: progetto3_nuovo,
+    url: 'https://dexy98.github.io/tecnostampa/',
+  },
 
-  export default data
+  // Aggiungi altri progetti qui...
+];
+
+export default data
