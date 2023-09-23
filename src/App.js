b69f685fbf85d6b5c131dc45b2f46bf06832baee
Dexy98 <davidecandia@live.it>
2023-09-23 12:33:10 +0200

    tolto loading iniziale. problemi con smartphone

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Home from './components/Home';
 import Nav from './components/Nav';
@@ -6,29 +6,20 @@ import Competenze from './components/Competenze';
 import data from './data';
 import Contatti from './components/Contatti';
 import Footer from './components/Footer';
-import Loading from './components/Loading';
 
 function App() {
   const [progetti, setprogetti] = useState(data);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    window.onload = () => {
-      setLoading(false);
-    };
-  }, []);
 
   return (
     <>
-      {loading ? <Loading /> : (
-        <>
-          <Nav />
-          <Home />
-          <Competenze data={progetti} />
-          <Contatti />
-          <Footer />
-        </>
-      )}
+      <>
+        <Nav />
+        <Home />
+        <Competenze data={progetti} />
+        <Contatti />
+        <Footer />
+      </>
     </>
   );
 }
