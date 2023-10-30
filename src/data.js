38c45fe726bbd8264678426163a04d15c7273177
Dexy98 <davidecandia@live.it>
2023-10-30 15:41:44 +0100

    modificato disposizione progetti. aggiunto snake game

--- src/data.js
+++ src/data.js
@@ -3,11 +3,10 @@ import dog from "./assets/image/dog.png"
 import shop from "./assets/image/shop.png"
 import movies from "./assets/image/movies.png"
 import meteo from "./assets/image/meteo.jpg"
-import sinisky from "./assets/image/sinisky.jpg"
-import lista from "./assets/image/lista.jpg"
-import tecnostampa from "./assets/image/tecnostampa.jpg"
 import spotify_clone from "./assets/image/spotify.png"
 import notes from "./assets/image/notes.png"
+import snake from "./assets/image/snake.png"
+
 
 //img grandi
 import cocktail_open from './assets/image/cocktail-open.png'
@@ -16,10 +15,22 @@ import movies_open from './assets/image/movies-open.png'
 import notes_open from './assets/image/notes-open.png'
 import spotify_open from './assets/image/spotify-open.png'
 import shop_open from './assets/image/shop-open.png'
+import snake_open from './assets/image/snake-open.png'
 
 
 
 const data = [
+  {
+    id: '11',
+    name: 'Snake game',
+    image_open: snake_open,
+    mini_descrizione: 'Portale dove puoi giocare al classico game snake',
+    descrizione: 'Ho sviluppato questo progetto per esercitarmi a sviluppare un gioco con l\'utilizzo di canvas',
+    tecnologie: 'Sviluppato con HTML e principalmente javascript',
+    image: snake,
+    url_git: 'https://github.com/Dexy98/Snake-game',
+    url: 'https://snakegamegg.netlify.app/',
+  },
   {
     id: '10',
     name: 'Notes',
@@ -30,17 +41,6 @@ const data = [
     image: notes,
     url_git: 'https://github.com/Dexy98/todo_list',
     url: 'https://todo-6pnz.onrender.com/',
-  },
-  {
-    id: '9',
-    name: 'Spotify-clone',
-    image_open: spotify_open,
-    mini_descrizione: 'Portale clone di Spotify',
-    descrizione: 'Ho sviluppato questo clone di Spotify per esercitarmi con tailwind',
-    tecnologie: 'Sviluppato con vite come bundler di react. Tailwind per il css.',
-    image: spotify_clone,
-    url_git: 'https://github.com/Dexy98/Spotify-clone',
-    url: '',
   },
   {
     id: '8',
@@ -54,15 +54,15 @@ const data = [
     url: 'https://cocktailcreationshub.netlify.app/',
   },
   {
-    id: '7',
-    name: 'Dog',
-    image_open: dog_open,
-    mini_descrizione: 'Portale dove puoi visionare immagini di cani in base la razza',
-    descrizione: 'Ho sviluppato questo progetto per divertimento, mi è stato utile per capire meglio redux toolkit query.',
-    tecnologie: 'Sviluppato con React e Typescript. Tailwind per il css. API gestita con RTK query. ',
-    image: dog,
-    url_git: 'https://github.com/Dexy98/dog',
-    url: 'https://dexy98.github.io/dog/',
+    id: '9',
+    name: 'Spotify-clone',
+    image_open: spotify_open,
+    mini_descrizione: 'Portale clone di Spotify',
+    descrizione: 'Ho sviluppato questo clone di Spotify per esercitarmi con tailwind',
+    tecnologie: 'Sviluppato con vite come bundler di react. Tailwind per il css.',
+    image: spotify_clone,
+    url_git: 'https://github.com/Dexy98/Spotify-clone',
+    url: '',
   },
   {
     id: '6',
@@ -87,6 +87,17 @@ const data = [
     url: 'https://dexy98.github.io/movies/',
   },
   {
+    id: '7',
+    name: 'Dog',
+    image_open: dog_open,
+    mini_descrizione: 'Portale dove puoi visionare immagini di cani in base la razza',
+    descrizione: 'Ho sviluppato questo progetto per divertimento, mi è stato utile per capire meglio redux toolkit query.',
+    tecnologie: 'Sviluppato con React e Typescript. Tailwind per il css. API gestita con RTK query. ',
+    image: dog,
+    url_git: 'https://github.com/Dexy98/dog',
+    url: 'https://dexy98.github.io/dog/',
+  },
+  {
     id: '4',
     name: 'meteo',
     image_open: '',
@@ -97,39 +108,7 @@ const data = [
     url_git: 'https://github.com/Dexy98/meteo',
     url: 'https://dexy98.github.io/meteo/',
   },
-  {
-    id: '3',
-    name: 'sinisky',
-    image_open: '',
-    mini_descrizione: 'Portale di un attività tech',
-    descrizione: 'Ho sviluppato questo sito per un attività commerciale nel mondo del tech',
-    tecnologie: 'Sviluppato con React',
-    image: sinisky,
-    url_git: 'https://github.com/Dexy98/sinisky',
-    url: 'https://dexy98.github.io/sinisky/',
-  },
-  {
-    id: '2',
-    name: 'Lista',
-    image_open: '',
-    mini_descrizione: 'Portale per gestire le tue note salvate in locale',
-    descrizione: 'Ho realizzato questo progetto come esercitazione, è stato utile per capire come gestire gli state in react',
-    tecnologie: 'React js',
-    image: lista,
-    url_git: 'https://github.com/Dexy98/lista',
-    url: 'https://dexy98.github.io/lista/'
-  },
-  {
-    id: '1',
-    name: 'tecnostampa',
-    image_open: '',
-    mini_descrizione: 'Portale di una tipografia',
-    descrizione: 'Realizzazione di un sito web informativo per una tipografia',
-    tecnologie: 'Sviluppato con react js',
-    image: tecnostampa,
-    url_git: 'https://github.com/Dexy98/tecnostampa',
-    url: 'https://dexy98.github.io/tecnostampa/',
-  },
+
 
   // Aggiungi altri progetti qui...
 ];
