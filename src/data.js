6e323368b87406d03bc17c0ad765c4929b5dcd3a
Dexy98 <davidecandia@live.it>
2023-10-31 10:15:26 +0100

    cambiato nome utente GIT

--- src/data.js
+++ src/data.js
@@ -28,7 +28,7 @@ const data = [
     descrizione: 'Ho sviluppato questo progetto per esercitarmi a sviluppare un gioco con l\'utilizzo di canvas',
     tecnologie: 'Sviluppato con HTML e principalmente javascript',
     image: snake,
-    url_git: 'https://github.com/Dexy98/Snake-game',
+    url_git: 'https://github.com/davidecandia/Snake-game',
     url: 'https://snakegamegg.netlify.app/',
   },
   {
@@ -39,7 +39,7 @@ const data = [
     descrizione: 'Ho sviluppato questo progetto per esercitarmi a gestire dati e utenti connessi al database, quello che ho appreso è rendere delle route private, accessibili solo quando un utente è loggato, catturare le note create dall\'utente e quindi avere una lista privata di note.',
     tecnologie: 'Sviluppato con lo stack MERN, MongoDB: Come database, Express.js: Come framework per il server, React: Per il frontend, Node.js: Per l\'esecuzione del server. Typescript sia per frontEnd che BackEnd. Vite come bundler di react. Css con tailwind + gestione delle richieste al server con Redux query.',
     image: notes,
-    url_git: 'https://github.com/Dexy98/todo_list',
+    url_git: 'https://github.com/davidecandia/todo_list',
     url: 'https://todo-6pnz.onrender.com/',
   },
   {
@@ -50,7 +50,7 @@ const data = [
     descrizione: 'Ho sviluppato questo progetto per esercitarmi su alcune tecnologie. L’idea è di una piattaforma che permetta di cercare il proprio cocktail preferito. È presente un campo di ricerca e in basso verrano visualizzati i risultati. Ogni singolo cocktail è cliccabile e l’utente viene spostato in una pagina dove può visionare tutti i dettagli relativi al drink.',
     tecnologie: 'Sviluppato con React e Typescript. Tailwind per il css. Dati presi da \'thecocktaildb.com/api.php\' un Api gratuita ed ho gestito le chiamate al server tramite Redux toolkit Query',
     image: cocktail,
-    url_git: 'https://github.com/Dexy98/thecocktaildb',
+    url_git: 'https://github.com/davidecandia/thecocktaildb',
     url: 'https://cocktailcreationshub.netlify.app/',
   },
   {
@@ -61,7 +61,7 @@ const data = [
     descrizione: 'Ho sviluppato questo clone di Spotify per esercitarmi con tailwind',
     tecnologie: 'Sviluppato con vite come bundler di react. Tailwind per il css.',
     image: spotify_clone,
-    url_git: 'https://github.com/Dexy98/Spotify-clone',
+    url_git: 'https://github.com/davidecandia/Spotify-clone',
     url: '',
   },
   {
@@ -72,8 +72,8 @@ const data = [
     descrizione: 'Ho sviluppato questo progetto per esercitarmi nella creazione di uno store, dove puoi scegliere il prodotto aggiungerlo al carrello e gestire il carrello aumentando quantità o rimuovendo il prodotto inserito.',
     tecnologie: 'Sviluppato con React e tailwind per il css. Fake Store API gestita con RTK query. Gestione degli stati con redux toolkit e typescript per migliore gestione del codice. ',
     image: shop,
-    url_git: 'https://github.com/Dexy98/e-shop',
-    url: 'https://dexy98.github.io/e-shop/',
+    url_git: 'https://github.com/davidecandia/e-shop',
+    url: 'https://davidecandia.github.io/e-shop/',
   },
   {
     id: '5',
@@ -83,8 +83,8 @@ const data = [
     descrizione: 'Ho sviluppato questo progetto per esercitarmi nella creazione di un portale di contenuti multimediali, dati sempre aggiornati sulle nuove uscite e possibiltà di vedere il trailer se disponibile in su youtube in lingua italiana altrimenti in inglese.',
     tecnologie: 'Sviluppato con React e tailwind per il css. Fetch API TMDB',
     image: movies,
-    url_git: 'https://github.com/Dexy98/movies',
-    url: 'https://dexy98.github.io/movies/',
+    url_git: 'https://github.com/davidecandia/movies',
+    url: 'https://davidecandia.github.io/movies/',
   },
   {
     id: '7',
@@ -94,8 +94,8 @@ const data = [
     descrizione: 'Ho sviluppato questo progetto per divertimento, mi è stato utile per capire meglio redux toolkit query.',
     tecnologie: 'Sviluppato con React e Typescript. Tailwind per il css. API gestita con RTK query. ',
     image: dog,
-    url_git: 'https://github.com/Dexy98/dog',
-    url: 'https://dexy98.github.io/dog/',
+    url_git: 'https://github.com/davidecandia/dog',
+    url: 'https://davidecandia.github.io/dog/',
   },
   {
     id: '4',
@@ -105,8 +105,8 @@ const data = [
     descrizione: 'Ho realizzato questo progetto per esercitarmi, ed è stato stimolante gestire i dati soprattuto poter modificare l\'orario in modo da visualizzare \'oggi\' al posto della data corrente e orario vicino.',
     tecnologie: 'React js, tailwind per il css. Dati presi da openweathermap API. ',
     image: meteo,
-    url_git: 'https://github.com/Dexy98/meteo',
-    url: 'https://dexy98.github.io/meteo/',
+    url_git: 'https://github.com/davidecandia/meteo',
+    url: 'https://davidecandia.github.io/meteo/',
   },
 
 
